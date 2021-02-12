040fe05ce116272f9a187a720650874a9aa9bf1f
mnxmnz <codi0703@naver.com>
2021-02-13 02:27:23 +0900

    feat: paint 기능 생성

---
+++ app.js
@@ -0,0 +1,38 @@
+const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.55;
+
+let painting = false;
+
+const startPainting = event => {
+  painting = true;
+};
+
+const stopPainting = event => {
+  painting = false;
+};
+
+const onMouseMove = event => {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+};
+
+if (canvas) {
+  canvas.addEventListener('mousemove', onMouseMove);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
+  canvas.addEventListener('mouseleave', stopPainting);
+}
